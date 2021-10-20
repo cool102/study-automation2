dd4d9d0d7d6131b6534285cd9a11fe7ce24ccc42
cool102 <68699015+cool102@users.noreply.github.com>
2021-10-20 12:39:15 +0300

    Написал тесты для TestCase 2 - Авторизация подтвержденным пользователем

---
+++ src/test/java/at/tests/ui/ActiveUserLogin.java
@@ -0,0 +1,52 @@
+package at.tests.ui;
+
+import at.study.redmine.model.user.User;
+import lombok.SneakyThrows;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+
+public class ActiveUserLogin extends BaseUITest {
+    User user;
+
+    @BeforeMethod
+    public void prepareFixture() {
+
+        user = new User() {{
+
+        }}.create();
+
+        openBrowser();
+        headerPage.loginButton.click();
+        loginPage.login(user);
+
+    }
+
+    @Test(expectedExceptions = org.openqa.selenium.NoSuchElementException.class)
+
+    public void activeUserLoginTests() {
+        assertEquals(headerPage.homePage.getText(), "Домашняя страница");
+        assertEquals(headerPage.loggedAs.getText(), "Вошли как " + user.getLogin());
+        assertEquals(headerPage.myAccount.getText(), "Моя учётная запись");
+        assertEquals(headerPage.homePage.getText(),"Домашняя страница");
+        assertTrue(headerPage.homePage.isDisplayed());
+        assertTrue(headerPage.myPage.isDisplayed());
+        assertTrue(headerPage.projects.isDisplayed());
+        assertTrue(headerPage.help.isDisplayed());
+        assertTrue(headerPage.myAccount.isDisplayed());
+        assertTrue(headerPage.logOut.isDisplayed());
+
+       assertFalse(headerPage.administration.isDisplayed());
+       assertFalse(headerPage.loginButton.isDisplayed());
+       assertFalse(headerPage.register.isDisplayed());
+
+       assertTrue(headerPage.findToolBar.isDisplayed());
+
+
+    }
+}
